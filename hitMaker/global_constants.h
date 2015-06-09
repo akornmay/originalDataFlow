c99b6927a95fe77b3d6171db3a9e1089a0916f34
Stefano Mersi <alkemyst@users.noreply.github.com>
2015-06-09 18:30:42 +0200

    Avoiding out-of-boundary pixels

---
+++ hitMaker/global_constants.h
@@ -0,0 +1,11 @@
+#ifndef GLOBAL_CONSTANTS_H
+#define GLOBAL_CONSTANTS_H
+
+const int nColumns = 52;
+const int nRows = 80;
+const double pixelLength_ = 0.015; // cm
+const double pixelWidth_  = 0.010; // cm
+const double clockSpeed_  = 40;    // MHz
+const short defaultAdc = 50;
+
+#endif
